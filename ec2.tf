2392ac4bea08999b29366aea1e79f00d1670626a
Sganesh-30 <106546335+Sganesh-30@users.noreply.github.com>
2023-06-16 15:02:50 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web" {
-    ami           = ""
-    instance_type =
-    key_name      =
+    ami           = "ami-022e1a32d3f742bd8"
+    instance_type = "t2.micro"
+    key_name      = "project-1"
     subnet_id     = aws_subnet.public[count.index].id
     vpc_security-group = [aws_security_group.allow_tls.id]
     associate_public_ip_address = true
@@ -12,22 +12,22 @@ resource "aws_instance" "web" {
     }
 
     provisioner "file" {
-        source= ""
-        destination = ""
+        source= "./project-1"
+        destination = "/home/ec3-user/project-1"
 
         connection {
             type = "ssh"
             host = self.public.ip
             user = "ec2-user"
-            private_key = ""
+            private_key = "${file("./project-1")}
         }
     }
 }
 
 resource "aws_instance" "web" {
-    ami           = ""
-    instance_type =
-    key_name      =
+    ami           = "ami-022e1a32d3f742bd8"
+    instance_type = "t2.micro"
+    key_name      = "project-1"
     subnet_id     = aws_subnet.public[count.index].id
     vpc_security-group = [aws_security_group.allow_tls.id]
 
