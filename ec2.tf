751e98150092ce8b9aac7ec48f5e0cebedc98546
Sganesh-30 <106546335+Sganesh-30@users.noreply.github.com>
2023-06-23 10:47:20 +0530

    Created ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,30 @@
+resource "aws_instance" "web" {
+  ami           = ""
+  instance_type = "t2.micro"
+  key_name = "TF_key"
+  subnet_id = aws_subnet.public[count.index].id
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  associate_public_ip_address = true
+  count = 2
+  }
+resource "aws_key_pair" "TF_key" {
+  key_name = "TF_key"
+  public_key = tls_private_key.rsa.public_key_openshh
+}
+
+resource "tls_private_key" "rsa" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_instance" "db" {
+  ami           = ""
+  instance_type = "t2.micro"
+  key_name = "TF_key"
+  subnet_id = aws_subnet.private.id
+  vpc_security_group_ids = [aws_security_group.allow_tls_db.id]
+
+  tags = {
+    Name = "DB Server"
+  }
+}
