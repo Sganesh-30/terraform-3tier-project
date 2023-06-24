d654a17dae0fdd7a8113f16d20c2814f88cf3d48
Sganesh-30 <106546335+Sganesh-30@users.noreply.github.com>
2023-06-24 12:11:14 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "aws" {
-  access_key = "AKIAZTEEZBO4GXYEYMYH"
-  secret_key = "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"
+  access_key = "AKIAZTEEZBO4NQGJCA4U"
+  secret_key = "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"
   region = "ap-south-1"
 }
